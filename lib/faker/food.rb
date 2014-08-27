ae56271df17c1a4701bb02516c8ad98e2bc76442
Ruthan <rcfreese@gmail.com>
2014-08-27 15:47:08 -0400

    Add dish methods; use fetch instead of parse to match updated yml

--- lib/faker/food.rb
+++ lib/faker/food.rb
@@ -2,7 +2,23 @@ module Faker
   class Food < Base
     class << self
       def bento
-        parse('food.bento.bento')
+        fetch('food.bento.main') + ' with ' + fetch('food.bento.side') + ', ' + fetch('food.bento.filler') + ', and ' + fetch('food.bento.starch')
+      end
+
+      def maindish(type) # meat or veg
+        fetch('food.style') + ' ' + fetch("food.#{type}") + ' ' + fetch('food.format')
+      end
+
+      def fancify
+        fetch('food.flavor') + ' ' + fetch('food.garnish')
+      end
+
+      def fancydish(*args)
+        if args[1] == 'hipster'
+          fetch('food.hipster') + ' ' + self.maindish(args[0]) + ' with ' + fetch('food.hipster') + ' ' +  self.fancify
+        else
+          self.maindish(args[0]) + ' with ' + self.fancify
+        end
       end
     end
   end
